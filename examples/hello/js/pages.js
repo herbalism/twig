83584edbfbd470f2cfb98ca7aacccb9ac0d235f1
Joakim Ohlrogge <joakim.ohlrogge@agical.com>
2012-10-07 10:34:02 +0200

    initial scaffolding for showing pages

--- examples/hello/js/pages.js
+++ examples/hello/js/pages.js
@@ -1,4 +1,5 @@
 define(['twig', 'twig!hello'], 
        function(twig) {
+	   console.log("About to start twig", twig);
 	   twig.start('hello');
        });
--- src/twig.js
+++ src/twig.js
@@ -1,7 +1,21 @@
-define([], function() {
+define(["require", "twig-base"], function(require, base) {
     return {
-	load: function() {
-	    console.log("load twig");
+	load: function(resourceName, req, callback, config) {
+	    req([resourceName], function(page) {
+		callback({
+		    show: function(element) {
+			var element = element || 'body';
+			page(element, base.document);
+		    }
+		})
+	    })
+	},
+	start: function(name) {
+	    console.log("showing twig: ", name);
+	    require(["twig!"+name], function(twig) {
+		console.log("about to show", twig);
+		twig.show();
+	    })
 	}
     }
 });
